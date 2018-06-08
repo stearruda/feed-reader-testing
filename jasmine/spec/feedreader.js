d6f5d8266fa1b5d3e0b1ccdf4fad9c333aa7bdad
Stephanie Oliveira <stearruda@gmail.com>
2018-06-08 22:17:40 +0200

    Add test scenario for feed URL

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('have in each feed a defined url', function() {
+            for (let i in allFeeds) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(typeof allFeeds[i].url).toBe('string');
+                expect(allFeeds[i].url).not.toBe('');
+            };
+        });
 
 
         /* TODO: Write a test that loops through each feed
